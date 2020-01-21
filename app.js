70acb7e061ad37809458174f149048f59724fccc
lmoody <l.moody@continuumonline.com>
2020-01-21 00:07:08 -0700

    updating app.js to make inquirer calls, also updated lib objects extended from employee class

--- app.js
+++ app.js
@@ -1,47 +1,158 @@
-// Create 4 classes: Employee, Manager, Engineer, Intern.
-// The tests for these classes in the tests directory must all pass.
-// The first class is an Employee parent class with the following properties and
-// methods:
-
-        // name
-        // id
-        // title
-        // getName()
-        // getId()
-        // getEmail()
-        // getRole() // Returns 'Employee'
-
-// The other three classes will extend Employee.
-// In addition to Employee's properties and methods, Manager will also have:
-
-        // officeNumber
-        // getRole() // Overridden to return 'Manager'
-
-
-// In addition to Employee's properties and methods, Engineer will also have:
-
-        // github  // GitHub username
-        // getGithub()
-        // getRole() // Overridden to return 'Engineer'
-
-// In addition to Employee's properties and methods, Intern will also have:
-
-        // school
-        // getSchool()
-        // getRole() // Overridden to return 'Intern'
-
-// Keep these lines; they're important!
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util")
 
 const outputPath = path.resolve(__dirname, "output", "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const team = [];
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class teamBuilder {
+    confirmManager() {
+        inquirer
+            .prompt([
+                {
+                    type: "confirm"
+                    , message: "Are you the team manager?"
+                    , name: "managerConfirm"
+                }
+            ]).then(res => {
+                if (res.managerConfirm) {
+                    this.createManager()
+                } else {
+                    console.log("You need to be a manager to create a team!")
+                    process.exit(0);
+                }
+            })
+    };
+
+    createManager() {
+        inquirer
+            .prompt([
+                {
+                   type: "input"
+                   , message: "What is your name?"
+                   , name: "managerName"
+                },
+                {
+                   type: "input"
+                   , message: "What is your ID?"
+                   , name: "managerID"
+                },
+                {
+                   type: "input"
+                   , message: "What is your office phone?"
+                   , name: "managerPhone"
+                },
+                {
+                   type: "input"
+                   , message: "What is your office email?"
+                   , name: "managerEmail"
+                },
+            ]).then(res => {
+                const manager = new Manager(res.managerID, res.managerName, res.managerEmail, res.managerPhone);
+                team.push(manager);
+                this.createTeam();
+            });
+    };
+
+    createTeam() {
+        inquirer
+            .prompt([
+                {
+                    type: "list"
+                    , message: "What teams members would you like to create?"
+                    , choices: ["Engineer", "Intern", "I'm done Adding Team Members"]
+                    , name: "employeeType"
+                }
+            ]).then(res => {
+                if (res.employeeType ==="Engineer"){
+                    this.createEngineer();
+                } else if (res.employeeType === "Intern") {
+                    this.createIntern();
+                } else {
+                    console.log(`Team Members: ${JSON.stringify(team)}`);
+                    render(team);
+                    createTeamHTML(html);
+
+                }
+            });
+    };
+
+    createEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: "input"
+                    , message: "What is your engineer's name?"
+                    , name: "engineerName"
+                },
+                {
+                    type: "input"
+                    , message: "What is the engineer's ID?"
+                    , name: "engineerID"
+                },
+                {
+                    type: "input"
+                    , message: "What is the engineer's gitlab username?"
+                    , name: "engineerPhone"
+                },
+                {
+                    type: "input"
+                    , message: "What is the engineer's office email?"
+                    , name: "engineerEmail"
+                },
+            ]).then(res => {
+                const engineer = new Engineer(res.engineerID, res.engineerName, res.engineerEmail, res.engineerPhone);
+                team.push(engineer);
+                this.createTeam();
+            });
+    };
+
+    createIntern() {
+        inquirer
+            .prompt([
+                {
+                    type: "input"
+                    , message: "What is the inter's name?"
+                    , name: "internName"
+                },
+                {
+                    type: "input"
+                    , message: "What is the inter's ID?"
+                    , name: "internID"
+                },
+                {
+                    type: "input"
+                    , message: "What is the inter's school?"
+                    , name: "internSchool"
+                },
+                {
+                    type: "input"
+                    , message: "What is the inter's office email?"
+                    , name: "internEmail"
+                },
+            ]).then(res => {
+                const intern = new Intern(res.internID, res.internName, res.internEmail, res.internSchool);
+                team.push(intern);
+                this.createTeam();
+            });
+    }
+
+    async createTeamHTML(html) {
+        await writeFileAsync(outputPath, html);
+    };
+}
+
+const appStart = new teamBuilder();
+
+appStart.confirmManager();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
